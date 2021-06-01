e550f5f6720af2fd4079146a8136f7b2848f1a3e
sbhambr1 <sbhambr1@asu.edu>
2021-05-31 17:47:32 -0700

    sample runs for DOBSS and outputs generated

--- src/DOBSS/run.sh
+++ src/DOBSS/run.sh
@@ -4,7 +4,7 @@
 
 # Use the one your heart desires. Comment out the rest.
 gurobi.sh BSG_miqp.py input.txt > output.txt
-#gurobi.sh BSG_milp.py input.txt > output.txt
+# gurobi.sh BSG_milp.py input.txt > output.txt
 #gurobi.sh BSG_vs_UR.py input.txt >output.txt
 
 # Print output to terminal
